d322ad93d8a007a33768dc7c850c74722162ff69
Junio da Silva Damasceno <juniowin@yahoo.com.br>
2021-03-11 22:01:51 -0300

    Olá Mundo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nHello World!\n");
+            Console.WriteLine("Este programa foi criado para ensinar a imprimir informações na tela\n");
+        }
+    }
+}
